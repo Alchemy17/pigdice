1ce39561a5273e7c82e530ee86559e56f2891203
Abdulrahman Mohamed <maanoatu@gmail.com>
2018-03-04 11:25:22 +0300

    working on the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,73 @@
 //Backend
 var player1 = ""
 var player2 = ""
+
+
+function Player(playername, score) {
+  this.playername = playername;
+  this.score = score;
+}
+
+
+function nameAndScore() {
+  $("#p1Name").text(p1.playername);
+  $("#p2Name").text(p2.playername);
+  $("#p1TotalScore").text(p1.score);
+  $("#p2TotalScore").text(p2.score);
+}
 var playersturn = []
 
-function Player(player1name, player2name, score1, score2){
-  this.player1name = player1name;
-  this.player2name = player2name;
-  this.score1 = score1;
-  this.score2 = score2;
+function issaOne() {
+  if ($("#player1turn").is(":visible")) {
+    playersturn = [0];
+    $("#total1").text(playersturn);
+    alert("Oops! Issa One");
+    $("#player1turn").hide();
+    $("#player2turn").text(p1.playername + "'s turn").show();
+
+  } else if ($("#player2turn").is(":visible")) {
+    playersturn = [0];
+    $("#total1").text(playersturn);
+    alert("Oops! Issa One");
+    $("#player2turn").hide();
+    $("#player1turn").text(p2.playername + "'s turn").show();
+  }
 }
-Player.prototype.totalscore = function(a, b){
-  return a+b;
-}
-function nameAndScore(){
-  $("#player1name").text(player1name);
-  $("#player2name").text(player1name);
-  $("")
+console.log(issaOne());
 
 
-}
 
 
 
 //Frontend
-$(document).ready(function(){
-  $("form").submit(function(event){
+$(document).ready(function() {
+  $("form").submit(function(event) {
     event.preventDefault();
-
-
-
-
+    var p1name = $("input#player1name").val();
+    var p2name = $("input#player2name").val();
+    score1 = 0;
+    score2 = 0;
+    p1 = new Player(p1name, score1);
+    p2 = new Player(p2name, score2);
+    nameAndScore();
     $("div.hide1").hide();
     $("div.gameMode").show();
-
+    $("input#roll").click(function(event) {
+      event.preventDefault();
+      $("die-roll").show();
+      var rolling = (Math.floor(Math.random() * 6 + 1));
+      $("#die-roll").text(rolling);
+      console.log(rolling);
+      if (rolling == 1) {
+        issaOne();
+      } else {
+        playersturn.push(rolling);
+        playersturn.reduce(function(a, b) {
+          return a + b;
+        }, 0);
+        $("#total1").text(playersturn);
+      }
+    });
   });
 
 
