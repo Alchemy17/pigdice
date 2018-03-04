48f4904609f5a1a3ddcac76fa8941c0a1dc21cb2
Abdulrahman Mohamed <maanoatu@gmail.com>
2018-03-04 16:04:05 +0300

    changes are made

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,7 @@ function issaOne() {
     $("#pl2").removeClass("pl1");
     $("#pl1").addClass("pl1");
     nameAndScore();
-  }
-  else if ($("#player2turn").is(":visible")) {
+  } else if ($("#player2turn").is(":visible")) {
     playersturn = [0];
     $("#total1").text(playersturn);
     alert("Oops! Issa One");
@@ -79,8 +78,8 @@ function switchPlayers() {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    var p1name = $("input#player1name") .val();
-    var p2name = $("input#player2name") .val();
+    var p1name = $("input#player1name").val();
+    var p2name = $("input#player2name").val();
     score1 = 0;
     score2 = 0;
     p1 = new Player(p1name, score1);
@@ -92,25 +91,27 @@ $(document).ready(function() {
     $("#pl2").addClass('pl1');
     $("#pl1").removeClass('pl1');
   });
-    $("input#roll").click(function(event) {
-      event.preventDefault();
-      $("#die-roll").show();
-      var rolling = (Math.floor(Math.random() * 6 + 1));
-      $("#die-roll").text(rolling);
-      if (rolling == 1) {
-        issaOne();
-      } else {
-        playersturn.push(rolling);
-        $("#result").text(playersturn.reduce(function(a, b) {return a + b}, 0));
-      }
-    });
-    $("#hold").click(function(event) {
-      switchPlayers();
-      if (p1.score >= 100) {
-        alert(p1.playername + " takes the cake!");
+  $("input#roll").click(function(event) {
+    event.preventDefault();
+    $("#die-roll").show();
+    var rolling = (Math.floor(Math.random() * 6 + 1));
+    $("#die-roll").text(rolling);
+    if (rolling == 1) {
+      issaOne();
+    } else {
+      playersturn.push(rolling);
+      $("#result").text(playersturn.reduce(function(a, b) {
+        return a + b
+      }, 0));
+    }
+  });
+  $("#hold").click(function(event) {
+    switchPlayers();
+    if (p1.score >= 100) {
+      alert(p1.playername + " takes the cake!");
 
-      } else if (p2.score >= 100) {
-        alert(p2.playername + " takes the cake!");
-      }
-    });
+    } else if (p2.score >= 100) {
+      alert(p2.playername + " takes the cake!");
+    }
+  });
 });
